57e85385fd06eaf5159d7a493ad8f11a17df41a4
Your Name <you@example.com>
2017-04-08 21:42:39 +0100

    1st commit

---
+++ lib/voicerss2017e.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+# file: voicerss2017e.rb
+
+require 'digest/md5'
+require 'voicerss2017'
+require 'fileutils'
+
+class VoiceRSSe
+ 
+  def initialize(api_key: nil, config: {}, cache_filepath: 'cache')
+
+    @config = ({
+      'key' => api_key,
+      'hl' => 'en-gb',
+      'src' => 'hello world',
+      'r' => '0',
+      'c' => 'mp3',
+      'f' => '44khz_16bit_stereo',
+      'ssml' => 'false',
+      'b64' => 'false'
+    }).merge config
+    
+    @cache_filepath = cache_filepath
+    
+  end
+
+  def tts(text='', audiofile_out='output.mp3')
+
+    FileUtils.mkdir_p @cache_filepath
+    
+    h = Digest::MD5.new << text
+    filename = File.join(@cache_filepath, h.to_s + '.mp3')
+    
+    
+    # attempts to find the audio file from a local cache instead of 
+    # making a relatively expensive request through the web API
+    
+    if File.exists? filename then
+      
+      FileUtils.cp filename, audiofile_out
+      
+    else
+            
+      voice = VoiceRSS.speech(@config.merge({'src' => text}))
+      File.write filename, voice['response']      
+
+    end    
+
+  end
+
+end
---
+++ voicerss2017e.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'voicerss2017e'
+  s.version = '0.1.0'
+  s.summary = 'An enhancement of the voicerss2017 gem by caching audio ' + 
+      'files locally to reduce the number of requests to the web API.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/voicerss2017e.rb']
+  s.add_runtime_dependency('voicerss2017', '~> 0.1', '>=0.1.0')
+  s.signing_key = '../privatekeys/voicerss2017e.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/voicerss2017e'
+end
